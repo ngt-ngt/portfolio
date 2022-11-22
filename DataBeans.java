d1dd2f80971080d384397927978931d396eb6d30
ngt-ngt <112692236+ngt-ngt@users.noreply.github.com>
2022-11-22 16:19:02 +0900

    Add files via upload
    サイトを参考にダンジョン探索ゲームを作ってみました。

---
+++ DataBeans.java
@@ -0,0 +1,39 @@
+package dungeon;
+
+import java.io.Serializable;
+
+public class DataBeans implements Serializable {
+	//フィールド値
+	private int x;
+	private int y;
+	private char[][] map;
+
+	//引数なしのコンストラクタ
+	public DataBeans() {
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public char[][] getMap() {
+		return map;
+	}
+
+	public void setMap(char[][] map) {
+		this.map = map;
+	}
+
+}
