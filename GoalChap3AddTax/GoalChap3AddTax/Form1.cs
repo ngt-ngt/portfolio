0f47b8be2c87bc9302804d64021450a068d931c3
ngt-ngt <112692236+ngt-ngt@users.noreply.github.com>
2023-01-03 23:17:56 +0900

    Visual C#テスト

---
+++ GoalChap3AddTax/GoalChap3AddTax/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoalChap3AddTax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a =int.Parse( textBox1.Text);
+            double result=a * 1.1;
+            label4.Text = result.ToString();
+        }
+    }
+}
